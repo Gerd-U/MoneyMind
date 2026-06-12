f875ba829e7d3aa4ce6c8bfb3897d4ab4f0269e9
Gerd-U <gerald.urbina45.3@gmail.com>
2026-06-12 13:15:34 -0600

    fix: use CSS grid to align footer with content column only

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
+    env: {
+      apiUrl: 'https://moneymind-be.onrender.com',
+    },
     setupNodeEvents(_on, _config) {
       return _config
     },
